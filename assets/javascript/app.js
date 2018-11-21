3912d564b8bf8f82b34b363bd24f472247873c1d
kathy zhang <kzhang193@gmail.com>
2018-11-20 23:10:55 -0600

    update draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,52 @@
 // Default button choices //
-var movies = ['Cinderella','The Little Mermaid','The Lion King', 'Mulan','Peter Pan', 'Monsters, Inc', 'Lilo and Stitch', 'Toy Story', 'Finding Nemo','The Incredibles', 'Wall-e', 'Ratatouille', 'Up'];
+var disneyMovies = ['Cinderella','The Little Mermaid','The Lion King', 'Mulan','Peter Pan', 'Monsters, Inc', 'Lilo and Stitch', 'Toy Story', 'Finding Nemo','The Incredibles', 'Wall-e', 'Ratatouille', 'Up'];
 
-function displayRating() {
+// new search by user
+$("button").on("click", function() {
 
-    var movie = $(this).attr("data-disney");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=S7N4zV82ub9Vv7Zw3iapPSxhQQbuI8uf&limit=20";
+    var disneyMovie = $(this).attr("data-disney");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + disneyMovie + "&api_key=S7N4zV82ub9Vv7Zw3iapPSxhQQbuI8uf&limit=20";
 
     $.ajax({
         url: queryURL,
         method: 'GET',
     }).then(function(response){
         
-        var movieDiv = response.data;
+   
+    var results = response.data;
 
-        for (var i = 0; i < movie.length; i++) {
+    for (var i = 0; i < results.length; i++) {
 
-            if (results[i].rating !=="r" && newMov[i].rating !== "pg-13") {
+        var gifDiv = $("<div>");
+        var rating  = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var movieGif = $("<img>");
+        movieGif.attr("src", results[i].images.fixed_height.url);
+        gifDiv.append(p);
+        gifDiv.append(movieGif);
+        $("#gif-holder").prepend(gifDiv);
+    }
+});
 
-                var gifDiv = $("<div>");
-                var rating = newMov[i].rating;
-
-                var ratingLabel = $("<p>").text("Rating: " + rating);
-
-                var movieGif = $("<img>");
-
-                movieGif.attr("src", newMov[i].images.fixed_height.url);
-
-                gifDiv.append(ratingLabel);
-                gifDiv.append(movieGif);
-
-
-                $("#gif-holder").prepend(gifDiv);
-            }
-        }
-    });
-};
-
-function renderbuttons(){
+function renderButtons() {
 
     $("#buttons-view").empty();
 
-    for (var i = 0; i < disneyMovs.length; i++); {
+    for (var i = 0; i < disneyMovies.length; i++); {
 
-        var add = $("<button>");
-        add.addClass("disney-btn");
-        add.attr("data-title", disneyMovs[i]);
-        add.text(disneyMovs[i]);
+        var add = $("<buttons>");
+        add.addClass("movie-btn");
+        add.attr("data-title", disneyMovies[i]);
+        add.text(disneyMovies[i]);
+
         $("#buttons-view").append(add);
-    }  
-}
+        }
+    }
 
-$("add-gif").on("click", function(event) {
-    event.preventDefault();
+    $("add-gif").on("click", function(event) {
+        event.preventDefault();
+        var disneyMovie = $("#disney-input").val().trim();
+        disneyMovies.push(disneyMovie);
+        renderButtons();
+    });
 
-    var disneyMov = $("#disney-input").val().trim();
-
-    disneyMovs.push(disneyMov);
-    render
-});
